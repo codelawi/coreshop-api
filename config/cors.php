9b2a3147a88b5d8c760d6fe593f8cadf531d72fb
codelawi <moathhijazi0@gmail.com>
2026-06-26 16:15:46 +0300

    editing cors.php

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
+        'https://dashboard.coreshop.io',
     ],
 
     'allowed_origins_patterns' => [],
