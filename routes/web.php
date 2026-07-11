4ffd98be7244571fb81cc543cc315c71abdb6fce
codelawi <moathhijazi0@gmail.com>
2026-07-11 11:53:06 +0300

    fixing blocking ip addresses

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-ip', function () {
+    return response()->json([
+        'laravel_ip' => request()->ip(),
+        'cf_connecting_ip' => request()->header('CF-Connecting-IP'),
+        'x_forwarded_for' => request()->header('X-Forwarded-For'),
+        'x_forwarded_proto' => request()->header('X-Forwarded-Proto'),
+    ]);
+});
