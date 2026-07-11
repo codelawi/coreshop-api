b6d32f8d9569c37b54b273ff27c5d0f9d30f88bb
codelawi <moathhijazi0@gmail.com>
2026-07-11 12:45:17 +0300

    v2 fixing blocking issues

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,13 @@ Route::get('/', function () {
 
 Route::get('/debug-ip', function () {
     return response()->json([
+        'remote_addr' => $_SERVER['REMOTE_ADDR'] ?? null,
         'laravel_ip' => request()->ip(),
         'cf_connecting_ip' => request()->header('CF-Connecting-IP'),
         'x_forwarded_for' => request()->header('X-Forwarded-For'),
         'x_forwarded_proto' => request()->header('X-Forwarded-Proto'),
+        'all_headers' => collect(request()->headers->all())
+            ->map(fn ($v) => $v[0])
+            ->toArray(),
     ]);
 });
